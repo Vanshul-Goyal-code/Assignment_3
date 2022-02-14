e08def7083296d03af137ea27d4a1630dcd05654
Vanshul-Goyal-code <96768035+Vanshul-Goyal-code@users.noreply.github.com>
2022-02-14 18:54:24 +0530

    Create output.py

---
+++ output.py
@@ -0,0 +1,93 @@
+Python 3.10.1 (v3.10.1:2cd268a3a9, Dec  6 2021, 14:28:59) [Clang 13.0.0 (clang-1300.0.29.3)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+
+=========== RESTART: /Users/vanshulgoyal/Python/Assignment3.py/Q1.py ===========
+Question 1
+
+Enter input string: my my name is is vanshul goyal
+my:2 
+name:1 
+is:2 
+vanshul:1 
+goyal:1 
+
+=========== RESTART: /Users/vanshulgoyal/Python/Assignment3.py/Q1.py ===========
+Question 1
+
+Enter input string: hello
+h:1 
+e:1 
+l:2 
+o:1 
+
+=========== RESTART: /Users/vanshulgoyal/Python/Assignment3.py/Q2.py ===========
+Question 2
+Day: 31
+Month: 12
+Year: 2020
+Next Date is: 1/1/2021
+
+=========== RESTART: /Users/vanshulgoyal/Python/Assignment3.py/Q3.py ===========
+Qustion 3
+[(3, 9), (7, 49), (9, 81), (10, 100)]
+
+=========== RESTART: /Users/vanshulgoyal/Python/Assignment3.py/Q4.py ===========
+Question 4
+Enter Grade Points: 5
+Your Grade is C and Below Average performance 
+
+=========== RESTART: /Users/vanshulgoyal/Python/Assignment3.py/Q5.py ===========
+Question 5
+ABCDEFGHIJK
+ ABCDEFGHI
+  ABCDEFG
+   ABCDE
+    ABC
+     A
+      
+
+=========== RESTART: /Users/vanshulgoyal/Python/Assignment3.py/Q6.py ===========
+Question 6
+Please Enter SID of Student- 21105079
+Please enter the name of the Student- vanshul goyal
+If you want to enter more details press Y/N- y
+Please Enter SID of Student- 21105078
+Please enter the name of the Student- vedansh singh
+If you want to enter more details press Y/N- y
+Please Enter SID of Student- 21105075
+Please enter the name of the Student- nimar singh
+If you want to enter more details press Y/N- n
+Part a
+SID 21105079 is of vanshul goyal
+SID 21105078 is of vedansh singh
+SID 21105075 is of nimar singh
+
+Part b
+value sorted dictionary is ['nimar singh', 'vanshul goyal', 'vedansh singh']
+
+Part c
+Keys sorted dictionary is [21105075, 21105078, 21105079]
+
+Part D
+Please enter the student's SID whose detail you need- 21105079
+Name of the required student is vanshul goyal
+
+=========== RESTART: /Users/vanshulgoyal/Python/Assignment3.py/Q7.py ===========
+Question 7
+Enter no. of terms: 10
+The Fibonacci sequence upto 10 terms is: 
+1,1,2,3,5,8,13,21,34,55,
+The average of the terms of Fibonacci sequence for 10 terms is 14.20
+
+=========== RESTART: /Users/vanshulgoyal/Python/Assignment3.py/Q8.py ===========
+Question 8
+(a) {1, 3, 5, 6, 8} 
+
+(b) {17, 3, 6, 8, 9, 13} 
+
+(c) {1, 2, 4, 5} 
+
+(d) {6, 7, 8, 9, 10} 
+
+(e) {10, 7} 
+
